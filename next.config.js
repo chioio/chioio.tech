f316ec49645691909a2a0553d64be73305624cea
Tenn Chio <Tenn_Chio@icloud.com>
2021-04-07 23:25:35 +0800

    ☺︎ deps: typescript & sass support

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
-module.exports = {
-  distDir: 'build',
-  env: {
-    DOMAIN: 'chioio.tech',
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-      },
-    ];
-  },
+const path = require('path')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+// Next config
+module.exports = (phase, { defaultConfig }) => {
+  return {
+    distDir: 'build',
+    env: {
+      DOMAIN: 'chioio.tech',
+    },
+    assetPrefix: isProd ? 'https://chioio.tech' : '',
+    async rewrites() {
+      return [
+        {
+          source: '/',
+          destination: '/home',
+        },
+      ];
+    },
+  };
 };
